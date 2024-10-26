8ac1fd1c2763b6115880f80307bfbfc1e1f61349
Fernanda <fernanda19@usf.edu>
2024-10-26 16:57:53 -0400

    Add event listener for product selection and quantity input

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ const placeOrderButton = document.getElementById('buy-button');  //  purchase bu
 const orderSummary = document.getElementById('order-summary');  //  order summary
 
 productSelector.addEventListener( "change", (event) => { //event listener to allow user to use dropdown menu
-const selectedProduct = event.target.value //this finds the product in the dropdown and corresponding price
-const quantity = (quantityInput.value);  // Get quantity
+const selectedProduct = parseFloat(event.target.value); //this finds the product in the dropdown and corresponding price
+const quantity = parseInt(quantityInput.value); // Get quantity (parsefloat and parseint make sure the value is treated as a number)
     const totalPrice = selectedProduct * quantity;  // Calculate total price
 totalPriceElement.textContent = `${totalPrice}` //updates total price according to qtd and product
 }
